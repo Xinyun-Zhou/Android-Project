755b9ea22d288c41156cf034af960aab70202714
Xinyun Zhou <u7326123@anu.edu.au>
2022-10-15 22:43:47 +1100

    PR: Merge the firebase

---
+++ app/src/main/java/com/example/mymarketplaceapp/fragments/LoginNotificationFragment.java
@@ -0,0 +1,35 @@
+package com.example.mymarketplaceapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mymarketplaceapp.R;
+
+/**
+ * The Login page in cart fragment
+ * Suggest the user to login before visit the Cart
+ * @author u7326123 Rita Zhou
+ */
+public class LoginNotificationFragment extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_notification, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.view = view;
+    }
+}
